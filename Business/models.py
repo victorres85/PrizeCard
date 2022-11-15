5c52da894863903b304b854f3b8868f82bdfee7e
victorres85 <victorres.almeida@gmail.com>
2022-11-15 13:35:21 +0000

    added Profile table

--- Business/models.py
+++ Business/models.py
@@ -3,14 +3,48 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
+import random 
+from .tasks import registration_completed
 
 # Create your models here.
+
+def confirmation_code():
+    code = ''
+    for i in range(15):
+        x = random.randint(34,91)
+        if i%2 == 0:
+            x = chr(x)
+        code += str(x)
+    return code     
+
+class Profile(models.Model):
+    confirmation = models.CharField(max_length= 300, default=confirmation_code(), null=True)
+    phone_number = models.IntegerField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    slug = models.SlugField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        registration_completed.delay(self.user.pk, self.confirmation)
+        return super().save(*args, **kwargs)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 
 class Businesses(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    slug = models.SlugField(blank=True, unique=True)
+    business_name = models.CharField(max_length = 200, blank=True)
     address_first_line = models.CharField(max_length = 200, blank=True)
     address_second_line = models.CharField(max_length = 200, blank=True)
     city = models.CharField(max_length = 100, blank=True) 
@@ -20,30 +54,24 @@ class Businesses(models.Model):
     logo = models.ImageField(upload_to='media/businesses/%y/%m/%d', blank=True)
     join_date = models.DateTimeField(auto_now_add = True)
     active = models.BooleanField(default=True, blank=True)
+   
 
     def getNumberOfActiveBusinesses(self):
         return Businesses.objects.filter(active=True).count()
 
     def getNumberOfBusinesses(self):
         return Businesses.objects.all().count()
-   
+
+    
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.user)
+            self.slug = slugify(self.business_name)
         return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.slug
 
 
-@receiver(post_save, sender=User)
-def create_business_profile(sender, instance, created, **kwargs):
-    if created:
-        Businesses.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_business_profile(sender, instance, **kwargs):
-    instance.businesses.save()
 
 
 class Cards(models.Model):
